d4caeb45525e2a15dd72cd11aef2c88e7a5387af
Carlos Henrique <ichfs9108@gmail.com>
2024-11-28 00:46:53 -0300

    feat: criando funcionalidade - CRUD

---
+++ todolist/src/main/java/tech/buildrun/todolist/Task.java
@@ -0,0 +1,4 @@
+package tech.buildrun.todolist;
+
+public record Task(Long id, String description) {
+}
---
+++ todolist/src/main/java/tech/buildrun/todolist/controller/dto/TaskDescription.java
@@ -0,0 +1,5 @@
+package tech.buildrun.todolist.controller.dto;
+
+public record TaskDescription(String description) {
+
+}
